6f265b4c95e831b8600342a6d0d639ee70c16a19
NovaStreamProg <gosha.panin.2011@ya.ru>
2017-01-30 20:02:15 +0700

    Update Hi-Cube.js

--- Hi-Cube.js
+++ Hi-Cube.js
@@ -1,19 +1,3 @@
-//стартовая мелодия
-var player = require('@amperka/ringtone').create(P1);
-
-var melody = 'Flinstones:d=4,o=5,b=40:32p,16f6,16a#,' +
-  '16a#6,32g6,16f6,16a#.,16f6,32d#6,32d6,32d6,32d#6,' +
-  '32f6,16a#,16c6,d6,16f6,16a#.,16a#6,32g6,16f6,16a#.,' + 
-  '32f6,32f6,32d#6,32d6,32d6,32d#6,32f6,16a#,16c6,a#,' + 
-  '16a6,16d.6,16a#6,32a6,32a6,32g6,32f#6,32a6,8g6,16g6,' + 
-  '16c.6,32a6,32a6,32g6,32g6,32f6,32e6,32g6,8f6,16f6,' +
-  '16a#.,16a#6,32g6,16f6,16a#.,16f6,32d#6,32d6,32d6,' +
-  '32d#6,32f6,16a#,16c.6,32d6,32d#6,32f6,16a#,16c.6,' +
-  '32d6,32d#6,32f6,16a#6,16c7,8a#.6';
- 
-player.play(melody).then(function() {
-  print('Melody completed');
-});
 //переменые
 var s = require('@amperka/light-sensor')
  .connect(A5);
